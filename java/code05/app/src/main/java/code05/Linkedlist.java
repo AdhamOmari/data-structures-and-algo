7453b574545d53f13a747f7e682de15760fd6567
AdhamOmari <adhamomarics@gmail.com>
2021-09-30 21:22:21 +0300

    solve

---
+++ java/code05/app/src/main/java/code05/Linkedlist.java
@@ -0,0 +1,39 @@
+package code05;
+
+public class Linkedlist {
+
+    Node head;
+    public void insert(int number) {
+        Node newNode = new Node(number);
+        if (head != null) {
+            newNode.next = head;
+        }
+        head = newNode;
+    }
+
+    public boolean includes(int value) {
+
+        Node curr = head;
+        while(curr != null){
+            if (curr.value == value){
+                return true;
+            }
+            curr=curr.next;
+        }
+        return false;
+    }
+
+    public  String toString(){
+        String printList = "";
+        Node current = head;
+        while (current!= null) {
+            printList = printList + "{ " + current.value + " } -> ";
+            if (current.next == null) {
+                printList = printList + "NULL";
+                return printList;
+            }
+            current = current.next;
+        }
+        return printList;
+    }
+}
