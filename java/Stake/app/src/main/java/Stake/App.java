dab6b34391b5ad19bea5845613b7dc642dccbd20
AdhamOmari <adhamomarics@gmail.com>
2021-10-11 19:31:43 +0300

    solve

--- java/Stake/app/src/main/java/Stake/App.java
+++ java/Stake/app/src/main/java/Stake/App.java
@@ -14,47 +14,63 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        stackDemo();
-        queueDemo();
+//        stackDemo();
+//        queueDemo();
+        pesudoqueueDemo();
     }
 
-    private static void stackDemo() {
-        Stack stack = new Stack();
+//    private static void stackDemo() {
+//        Stack stack = new Stack();
+//
+//        stack.push("A");
+//        stack.push("D");
+//        stack.push("H");
+//        stack.push("A");
+//        stack.push("M");
+//
+//        System.out.println("The top of the stack is => " + stack.peek());
+//
+//        System.out.println(stack.pop());
+//
+//        System.out.println("The top of the stack is => " + stack.peek());
+//
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//    }
 
-        stack.push("A");
-        stack.push("D");
-        stack.push("H");
-        stack.push("A");
-        stack.push("M");
+//    private static void queueDemo() {
+//        Queue queue = new Queue();
+//
+//        queue.enqueue("A");
+//        queue.enqueue("D");
+//        queue.enqueue("H");
+//        queue.enqueue("A");
+//        queue.enqueue("M");
+//
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println("The front of the Queue is => " + queue.peek());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//
+//
+//    }
+    private static void pesudoqueueDemo(){
+        Pseudo b=new Pseudo();
+        b.enqueue("A");
+        b.enqueue("L");
+        b.enqueue("I");
 
-        System.out.println("The top of the stack is => " + stack.peek());
+        b.enqueue("L");
+        System.out.println(b.dequeue().toString());
+        System.out.println(b.toString());
 
-        System.out.println(stack.pop());
 
-        System.out.println("The top of the stack is => " + stack.peek());
 
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-    }
-
-    private static void queueDemo() {
-        Queue queue = new Queue();
-
-        queue.enqueue("A");
-        queue.enqueue("D");
-        queue.enqueue("H");
-        queue.enqueue("A");
-        queue.enqueue("M");
-
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println("The front of the Queue is => " + queue.peek());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
     }
 }
