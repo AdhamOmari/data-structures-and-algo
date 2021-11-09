ec50460716d28c996f87f14a85af9cc368572e0f
AdhamOmari <adhamomarics@gmail.com>
2021-11-09 22:41:26 +0200

    solve

---
+++ java/CC33/README.md
@@ -0,0 +1,15 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+Write a function that LEFT JOINs two hashmaps into a single data structure.
+
+
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+
+![img](./ch33.png)
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+time : O(n) 
+space : O(n) 
--- java/HashTable/app/src/test/java/HashTable/AppTest.java
+++ java/HashTable/app/src/test/java/HashTable/AppTest.java
@@ -9,6 +9,8 @@ import treeIntersection.Tree;
 import treeIntersection.TreeNode;
 
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
@@ -50,5 +52,38 @@ class AppTest {
         secondBinaryTree.root.left = node8;
         assertEquals("[4]", Tree.treeIntersection(firstBinaryTree, secondBinaryTree).toString());
 
+    }
+    @Test
+    public void testLeftJoin() {
+        HashMap<String, String> h1 = new HashMap<>();
+        h1.put("fond", "enamored");
+        h1.put("wrath", "anger");
+        h1.put("diligent", "employed");
+        h1.put("outfit", "garb");
+
+
+        HashMap<String, String> h2 = new HashMap<>();
+        h2.put("fond", "averse");
+        h2.put("wrath", "delight");
+        h2.put("diligent", "idle");
+        h2.put("guide", "follow");
+
+
+        String[] a = {"enamored", "averse"};
+        String[] a1 = {"anger", "delight"};
+        String[] a2 = {"employed", "idle"};
+        String[] a3 = {"garb", null};
+        String[] a4 = {"usher", "follow"};
+
+        HashMap<String, String[]> h3 = new HashMap<>();
+
+        h3 = LeftJoin.leftJoin(h1, h2);
+        assertEquals(5, h3.size());
+        assertFalse(h3.containsKey("flow"));
+        assertArrayEquals(a, h3.get("fond"));
+        assertArrayEquals(a1, h3.get("wrath"));
+        assertArrayEquals(a2, h3.get("diligent"));
+        assertArrayEquals(a3, h3.get("outfit"));
+        assertArrayEquals(a4, h3.get("guide"));
     }
 }
